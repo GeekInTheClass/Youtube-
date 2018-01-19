759f4a640e5a9698e95de82f48aebfabc1fdd28f
bo5284 <35323734+bo5284@users.noreply.github.com>
2018-01-19 15:17:02 +0900

    잠자기5분전 테이블뷰

---
+++ 5minutesTable/5minutesTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  5minutesTable
+//
+//  Created by 방문사용자 on 2018. 1. 19..
+//  Copyright © 2018년 방문사용자. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
